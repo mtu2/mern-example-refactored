b6a86960e0c88bff4f4291b309729734a550442e
Michael <michael52423@gmail.com>
2021-01-09 23:36:15 +1100

    Fix from ages ago

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-  // Express will send React app if no API routes are hit
+  // Express will send React app if it doesn't recognise the route from API routes or from "client/build" directory
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
